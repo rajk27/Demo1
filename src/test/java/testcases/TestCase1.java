c49ad00c1daf5c3b3206d41cb565eb014a720c63
Raj Kamal <rakamal@bebotechnologies.com>
2022-07-15 11:39:40 +0530

    First version

---
+++ src/test/java/testcases/TestCase1.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void setup() {
+		System.out.println("This is Setup Test...");
+	}
+	@Test
+	public void login() {
+		System.out.println("This is login Test...");
+	}
+	
+}
